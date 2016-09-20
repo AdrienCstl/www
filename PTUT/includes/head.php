caa4a072e7f15a2fac6c956851f8b4b41adc111e
DemonazX <adrien.castellon@etu.univ-lyon1.fr>
2016-09-20 22:32:16 +0200

    add

---
+++ PTUT/includes/head.php
@@ -0,0 +1,10 @@
+	<head>
+		<title>Projet tuteuré</title>
+		
+		<meta name="language" content="fr" />
+		<link href="./style/header.css" media="all" rel="stylesheet " type="text/css" />
+		<link href="./style/footer.css" media="all" rel="stylesheet " type="text/css" />
+		<link href="./style/globalcss.css" media="all" rel="stylesheet " type="text/css" />
+		<link href="./style/index.css" media="all" rel="stylesheet " type="text/css" />
+		<script type="text/javascript" src="./scripts/jquery-1.12.3.js"></script>	
+	</head>
